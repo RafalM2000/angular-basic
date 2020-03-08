9fa9c1b26f08d743acf1078f9172f3dbae3173f8
Rafal <rmf@europe.com>
2020-03-08 14:35:13 +0100

    inline html and css

--- src/app/burger/burger.component.ts
+++ src/app/burger/burger.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '.app-burger',
+  selector: 'app-burger',
   templateUrl: './burger.component.html',
   styleUrls: ['./burger.component.css']
 })
--- src/app/vegeburger/vegeburger.component.ts
+++ src/app/vegeburger/vegeburger.component.ts
@@ -2,8 +2,18 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-vegeburger',
-  templateUrl: './vegeburger.component.html',
-  styleUrls: ['./vegeburger.component.css']
+  template: `<p class="container fine">VEGEBurger is OK</p>`,
+  styles: [
+    `	.container {
+      background-color: aqua;
+      padding: 20px;
+      border-radius: 10px;
+    }
+      .fine {
+        color: green;
+      }
+    `
+  ]
 })
 export class VegeburgerComponent implements OnInit {
 
